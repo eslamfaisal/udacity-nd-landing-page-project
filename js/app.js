e2ae7c85b959b04222dceb1321c6e5ccba8828cd
eslamfaisal <eslam.faisal.ef@gmail.com>
2021-11-22 01:19:52 +0200

    create nav list and append to html

--- js/app.js
+++ js/app.js
@@ -2,4 +2,35 @@
  * define variables
  */
 const sections = document.querySelectorAll('section');
+const navBarList = document.querySelector('#navbar__list');
 
+class MyApp  {
+
+    constructor() {
+        this.createNav();
+    }
+
+    /**
+     * create nav list based on sections
+     */
+    createNav() {
+        console.log("eslam faisal create nave")
+        const fragment = document.createDocumentFragment();
+        sections.forEach(section => {
+            const listItemTag = document.createElement("li")
+            const anchorTag = document.createElement("a")
+            anchorTag.innerText = section.getAttribute('data-sec-name');
+            anchorTag.classList.add("menu__link");
+            anchorTag.addEventListener("click", () => {
+                section.scrollIntoView({behavior: "smooth"});
+            });
+            listItemTag.appendChild(anchorTag);
+            fragment.appendChild(listItemTag);
+
+        });
+        navBarList.appendChild(fragment);
+    }
+
+}
+
+new MyApp()
